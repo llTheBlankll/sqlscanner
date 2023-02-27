26abbc803ed54172f827f2c822a423c3d277e7ac
Nytri <llTheBlankll@gmail.com>
2023-02-28 05:57:39 +0800

    Added threads argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading
+
 import scan
 import argparse
 import os
@@ -11,27 +12,28 @@ def clear():
         os.system("clear")
 
 
-def main(file: str):
+def main(file: str, threads: int):
     if file == "":
         print("No file was found.")
         return
 
     with open(file, "r") as f:
-        vulnerableHosts: list = []
-        notVulnerableHosts: list = []
+        vulnerable_hosts: list = []
+        not_vulnerable_hosts: list = []
+        thread_count: int = 0
         for url in f.readlines():
+            if thread_count >= threads:
+                print(threads)
+
+            thread_count += 1
             sc: scan.Scanner = scan.Scanner(url.strip("\n"))
-            scan_result: bool = sc.scan()
-            if scan_result is True:
-                vulnerableHosts.append(url)
-                print("%s : TRUE" % url)
-            else:
-                notVulnerableHosts.append(url)
-                print("%s : FALSE" % url)
+            sc.start()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", "-f", required=True, metavar="str")
+    parser.add_argument("--threads", "-t", metavar="int", default=10)
     args = parser.parse_args()
-    main(args.file)
+    print(type(args))
+    main(args.file, args.threads)
