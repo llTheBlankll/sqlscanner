804a0a0d06e3fefdcc4cd1c13b61d17894aae829
Nytri <llTheBlankll@gmail.com>
2023-03-05 21:39:36 +0800

    Added exception hosts

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import threading
-
+import prettytable
 import scan
 import argparse
 import os
@@ -16,7 +16,7 @@ def clear():
         os.system("clear")
 
 
-def main(file: str, thread_count: int, timeout: int):
+def main(file: str, thread_count: int, timeout: int, exception: str):
     if file == "":
         print("No file was found.")
         return
@@ -35,12 +35,15 @@ def main(file: str, thread_count: int, timeout: int):
                         not_vulnerable_hosts.append(url)
                     threads.remove(thread)
 
-            sc: scan.Scanner = scan.Scanner(url.strip(), timeout=timeout)
+            sc: scan.Scanner = scan.Scanner(url=url.strip(), timeout=timeout, exceptions=exception)
             sc.start()
             threads.append(sc)
 
         for thread in threads:
             thread.join()
+
+        for vulnerable_host in vulnerable_hosts:
+            print(vulnerable_host)
 
         print(f"{len(vulnerable_hosts)} VULNERABLE hosts and {len(not_vulnerable_hosts)} is NOT VULNERABLE")
 
@@ -50,5 +53,6 @@ if __name__ == "__main__":
     parser.add_argument("--file", "-f", required=True, metavar="File", dest="file")
     parser.add_argument("--threads", "-t", metavar="Thread", dest="threads", default=10, type=int)
     parser.add_argument("--timeout", "-a", metavar="Timeout", dest="timeout", default=30, type=int)
+    parser.add_argument("--exception", "-ex", metavar="Exception", dest="exception", type=str)
     args = parser.parse_args()
-    main(args.file, args.threads, args.timeout)
+    main(args.file, args.threads, args.timeout, args.exception)
--- scan.py
+++ scan.py
@@ -4,6 +4,7 @@ import requests
 import validators
 import colorama
 import termcolor
+import re
 
 user_agent: str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 " \
                   "Safari/537.36"
@@ -13,14 +14,24 @@ colorama.init()
 
 class Scanner(threading.Thread):
 
-    def __init__(self, url: str, timeout=30):
+    def __init__(self, url: str, exceptions: str, timeout=30):
         super().__init__()
 
+        self.exceptions = exceptions
         self.url = url
         self.result = False
         self.timeout: int = timeout
-
+        self.in_exception: bool = False
+        
         if validators.url(url.strip()):
+            with open(self.exceptions, "r") as f:
+                for host_exception in f.readlines():
+                    pattern = r"https?://(?:[a-zA-Z0-9]+[\-\.]?)*([a-zA-Z0-9\-]+)\.[a-zA-Z]{2,}(?:\/|$)"
+                    _domain1 = re.search(pattern, host_exception.strip()).group(1)
+                    _domain2 = re.search(pattern, self.url.strip()).group(1)
+                    if _domain1 == _domain2:
+                        self.in_exception = True
+
             # Create new session
             self.request_session = requests.Session()
 
@@ -36,6 +47,9 @@ class Scanner(threading.Thread):
 
     def run(self):
         try:
+            if self.in_exception:
+                return
+
             # Adds ' (single quote) for testing the sql injection
             request = requests.get(self.url + "'", timeout=self.timeout)
 
@@ -65,6 +79,8 @@ class Scanner(threading.Thread):
         except requests.ConnectionError as e:
             pass
         except TypeError as e:
+            pass
+        except requests.exceptions.InvalidSchema as e:
             pass
 
     def join(self, **kwargs) -> tuple:
